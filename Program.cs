c7fc94ca3908d8d5b974d4f7d11ee5bcfd9c9db3
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-10 13:16:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car1 = new Vehicle();
+            //Corsa car2 = new Corsa();
+            Console.WriteLine(car1.wheels);
+
+            Console.ReadLine();
+        }
+    }
+    abstract class Corsa
+    {
+        public int wheels = 4;
+    }
+    class Vehicle : Corsa
+    {
+        public int sparewheels = 1;
+    }
+
+
+
+
+}  
+
